515e199767527be91a9fffd3607d609eec8e5e9f
Ravali <ravali9sri@gmail.com>
2021-06-11 18:56:19 -0500

    commit

--- js/index.js
+++ js/index.js
@@ -6,7 +6,7 @@ $(document).ready(function () {
     var cartitems = [];
     var count;
     var prevcount = 0;
-    
+
 
 
     $('.card-link.add').click(function (e) {
@@ -63,14 +63,57 @@ $(document).ready(function () {
 
         itemname = $(this).parent().siblings('.card-title').text().trim();
         itemprice = $(this).parent().siblings('.card-text').children().text();
-        quant = $(this).parent().siblings('.form-select').val();
+        quant = parseInt($(this).parent().siblings('.form-select').val());
+
+        var itemobj = { name: itemname, price: itemprice, quantity: quant };
+
+
+        if ((cartitems.some(itemnamefound => itemnamefound.name === itemname) && (quant > 0))) {
+
+
+
+
+            console.log("found");
+            cartitems.find((o, i) => {
+                if (o.name === itemname) {
+                    var prevquant = parseInt(cartitems[i].quantity);
+                    if ((quant <= prevquant)) {
+
+                        cartitems[i] = { name: itemname, price: itemprice, quantity: prevquant - quant };
+
+                        var diffval = parseInt(cartitems[i].quantity);
+
+                        if (diffval == 0) {
+                            console.log("not found or removing");
+                            cartitems.pop(cartitems[i]);
+
+                        }
+
+
+
+                    }
+
+
+                }
+            });
+
+
+        }
+
+
+
+        else {
+
+            console.log("not applicable!")
+        }
 
 
         currentcount = quant;
         count = parseInt(prevcount) - parseInt(currentcount);
         prevcount = count;
-        //console.log(count);
-        $('.badge').text(count);
+        if (count > 0) {
+            $('.badge').text(count);
+        }
 
 
 
@@ -79,18 +122,19 @@ $(document).ready(function () {
 
     $('.cart').click(function (e) {
 
-        //e.preventDefault();
-        localStorage.setItem("cartitems",JSON.stringify(cartitems));
+        e.preventDefault();
+        localStorage.setItem("cartitems", JSON.stringify(cartitems));
         $.ajax({
-            
-           // url:"https://webhook.site/b5df8de1-1850-47a0-a19a-f5da84d1b832",
-           url: "cartitems.json",
+
+            url: "https://webhook.site/b5df8de1-1850-47a0-a19a-f5da84d1b832",
+            //url: "cartitems.json",
             type: "POST",
             data: JSON.stringify(cartitems),
             dataType: "json",
             success: function (response) {
+                window.location.href = "../html/cart.html";
                 console.log(response);
-                
+
             },
             error: function (error) {
                 console.log(error);
@@ -103,6 +147,74 @@ $(document).ready(function () {
 
     });
 
+
+
+
+
+
+
+
+
+
+
+
+});
+
+$(window).on('load', function (e) {
+
+    $.ajax({
+
+        url: "http://www.json-generator.com/api/json/get/ctVUzTrApu?indent=2",
+        //url: "cartitems.json",
+        type: "GET",
+
+        success: function (response) {
+            var cartdata;
+            $(response).each(function (o, i) {
+
+
+
+                name1 = i.name;
+                quant = i.quantity;
+                price = i.price;
+                var prevcart = $('.allitems').find('.cartitems').html();
+                console.log(prevcart);
+
+                $('.cartitems').find('.itemname').text(name1);
+                $('.cartitems').find('.itemquant').text(quant);
+                $('.cartitems').find('.itemprice').text(price);
+
+
+                
+                cartdata = $('.allitems').find('.cartitems').html();
+                console.log(cartdata);
+
+
+                if (cartdata.length > 0) {
+
+
+                    $('.allitems').append(prevcart+cartdata);
+
+                }
+
+                else {
+
+                    $('.allitems').append(cartdata);
+                }
+
+
+
+            });
+
+
+
+            console.log(response);
+
+        },
+        error: function (error) {
+            console.log(error);
+        }
+    });
 
 
 
